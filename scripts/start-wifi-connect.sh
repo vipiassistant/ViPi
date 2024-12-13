ff55f46b0d4fdd221f7d83321a70ad4c750b3d66
vipiassistant <vipi.tearm@gmail.com>
2024-12-13 22:00:46 +0700

    Add files via upload

---
+++ scripts/start-wifi-connect.sh
@@ -0,0 +1,6 @@
+- Kính gửi Anh!
+ Sau khi kiểm tra anh có gửi cho em PL thiết bị chưa khép kín quy trình, Em đã điều hành GĐ cụm thực hiện khép kín quy trình
+ Hiện tại em kiểm tra lại trên Phần mềm còn 19 Thiết bị chưa khép kín Quy trình (trong đó 16 thiết bị lỗi do SAP thiếu mã cha chưa khép kín quy trình , còn 3 thiết bị chưa được xử lý (chi tiết em gửi kèm theo file)
+- Kính nhờ Anh giảm trừ giúp số Thiết bị đã xử lý, còn lại đưa vào biên bản 13 Thiết bị với nội dung như trên!
+Cảm ơn Anh rất nhiều!
+ 
